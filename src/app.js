611c293dfc30edd9a8a2c24a3e5519775060a133
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-03-27 01:48:47 +0700

    feat(api): update CORS configuration to allow requests from any origin

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
